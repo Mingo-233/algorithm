452d80034497fd202a30a61b8a02ed4f9e12b09d
mingo <mingo233@qq.com>
2021-11-25 22:09:18 +0800

    feat

--- demo.js
+++ demo.js
@@ -1,73 +1,65 @@
-/*题目描述：设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
-
-push(x) —— 将元素 x 推入栈中。
-
-pop() —— 删除栈顶的元素。
-top() —— 获取栈顶元素。
-getMin() —— 检索栈中的最小元素。
-
-示例:
-
-MinStack minStack = new MinStack();
-minStack.push(-2);
-minStack.push(0);
-minStack.push(-3);
-minStack.getMin(); --> 返回 -3.
-minStack.pop();
-minStack.top(); --> 返回 0.
-minStack.getMin(); --> 返回 -2. */
-
-const MinStack = function(){
-  this.stack = []
-  //辅助栈 优化解法，记录最小值
-  this.stack2 = []
+function sleep (ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, ms);
+  })
 }
 
-MinStack.prototype.push = function(x){
-  this.stack.push(x)
 
-  if(this.stack2.length ===0 ||this.stack2[this.stack2.length-1] >x){
-    this.stack2.push(x)
+// 这样是可以实现所有回调函数同步运行的
+async function fun(cbs) {
+  for (let i = 0; i < cbs.length; i++) {
+    await cbs[i]()
   }
 }
-MinStack.prototype.pop = function(x){
-  //如果删除的stack2栈顶的数据，stack2也出栈（保持同步）
-  if(this.stack.pop(x)===this.stack2[this.stack2.length-1]){
-    this.stack2.pop()
-  }
+/* 使用foreach却不行 为什么？
+原因是forEach的原理是里面使用了while循环，每次循环都会执行一次回调函数，而下面这种写法，相当于执行了多次async await函数。
+注意：如何想要堵塞，是必须在一个async函数内部去堵塞，才能实现同步关系。例如 fun3内部的a函数和b函数
+而执行单独每个async函数，他们其实之间是异步关系。例如 async function fun  和async function fun2
+
+*/
+async function fun2(cbs) {
+  cbs.forEach(async(item) => {
+    await item()
+  });
   
 }
-MinStack.prototype.top = function(x){
-  // 取栈顶元素，这里本能地给它一个边界条件判断（其实不给也能通过，但是多做不错哈）
-  if(!this.stack || !this.stack.length) return
-  return this.stack[this.stack.length-1]
+
+async function fun3(){
+  await a()
+  await b()
+}
+/*
+所以，怎么用foreach实现我们想要的效果？
+答案是 我们手写一个
+*/
+
+Array.prototype.myForEach = async function (callback,thisArg ) {
+  //指向这个函数的调用者
+  const _arr = this,
+        _isArray = Array.isArray(thisArg),
+  //foreach的第二个参数，当执行回调函数 callback 时，用作 this 的值。
+        _thisArg = _isArray ? Object(thisArg):window
+
+  for (let i = 0; i < _arr.length; i++) {
+    await callback.call(_thisArg,_arr[i],i,_arr) 
+  } 
 }
 
-//常规解法 时间复杂度O(n)
-MinStack.prototype.getMin = function(x){
-  if(!this.stack || !this.stack.length) return
-  // 设一个非常大的值
-  let min = Infinity
-  for (let i = 0; i < this.stack.length; i++) {
-    if(this.stack[i]<min){
-      min = this.stack[i]
-    }
-  }
-  return min
+async function fun4(cbs) {
+  cbs.myForEach(async(fn) => {
+    await fn()
+  });
+  
 }
-//优化解法 时间复杂度O(1)
-MinStack.prototype.getMin2 = function(x){
-  if(!this.stack2 || !this.stack2.length) return
-  return this.stack2[this.stack2.length-1]
-}
-let minStack = new MinStack();
-minStack.push(-2);
-minStack.push(0);
-minStack.push(-3);
-console.log(minStack.getMin()); //-3.
-minStack.pop();
-console.log(minStack.top()); // 0.
-console.log(minStack.getMin()); // -2
-console.log(minStack.getMin2()); // -2
 
-
+fun4([
+  ()=>console.log('1'),
+  ()=>sleep(1000),
+  ()=>console.log('2'),
+  ()=>sleep(2000),
+  ()=>console.log('3'),
+  ()=>sleep(3000),
+  ()=>console.log('4'),
+])
